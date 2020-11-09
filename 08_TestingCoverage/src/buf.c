c769c1426a27748f2330c8ce2250ba595cc8a0a9
a.nikolaeva <nikolaevaytalina@gmail.com>
2020-11-09 17:02:36 +0300

    Fix 08_TestingCoverage

--- 08_TestingCoverage/src/buf.c
+++ 08_TestingCoverage/src/buf.c
@@ -1,32 +1,14 @@
 #include <stddef.h>
+#include <stdio.h>
 
 #include "buf.h"
 
-void * buf_grow1(void *v, size_t esize, ptrdiff_t n)
-{
-    struct buf *p;
-    size_t max = (size_t)-1 - sizeof(struct buf);
-    if (v) {
-        p = buf_ptr(v);
-        if (n > 0 && p->capacity + n > max / esize)
-            goto fail; /* overflow */
-        p = realloc(p, sizeof(struct buf) + esize * (p->capacity + n));
-        if (!p)
-            goto fail;
-        p->capacity += n;
-        if (p->size > p->capacity)
-            p->size = p->capacity;
-    } else {
-        if ((size_t)n > max / esize)
-            goto fail; /* overflow */
-        p = malloc(sizeof(struct buf) + esize * n);
-        if (!p)
-            goto fail;
-        p->capacity = n;
-        p->size = 0;
-    }
-    return p->buffer;
-fail:
-    BUF_ABORT;
+int main() {
+    float *values = 0;
+    for (size_t i = 0; i < 25; i++)
+        buf_push(values, rand() / (float)RAND_MAX);
+    for (size_t i = 0; i < buf_size(values); i++)
+        printf("values[%zu] = %f\n", i, values[i]);
+    buf_free(values);
     return 0;
 }
